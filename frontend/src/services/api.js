030ef6fcd46bdbbb3aa46096b46cfc361813fe04
jonatanalzate <jonatanalzate@hotmail.com>
2025-04-30 21:54:02 -0500

    Corrigiendo operaciones bulk para usar HTTPS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -84,34 +84,26 @@ export const api = {
   createConductor: (data) => axiosInstance.post('/conductores', data),
   updateConductor: (id, data) => axiosInstance.put(`/conductores/${id}`, data),
   deleteConductor: (id) => axiosInstance.delete(`/conductores/${id}`),
-  createConductoresBulk: async (data) => {
-    return axios.post(`${API_URL}/conductores/bulk`, data);
-  },
+  createConductoresBulk: (data) => axiosInstance.post('/conductores/bulk', data),
 
   // Vehículos
   getVehiculos: () => axiosInstance.get('/vehiculos'),
   createVehiculo: (data) => axiosInstance.post('/vehiculos', data),
   updateVehiculo: (id, data) => axiosInstance.put(`/vehiculos/${id}`, data),
   deleteVehiculo: (id) => axiosInstance.delete(`/vehiculos/${id}`),
-  createVehiculosBulk: async (data) => {
-    return axios.post(`${API_URL}/vehiculos/bulk`, data);
-  },
+  createVehiculosBulk: (data) => axiosInstance.post('/vehiculos/bulk', data),
 
   // Rutas
   getRutas: () => axiosInstance.get('/rutas'),
   createRuta: (data) => axiosInstance.post('/rutas', data),
   updateRuta: (id, data) => axiosInstance.put(`/rutas/${id}`, data),
   deleteRuta: (id) => axiosInstance.delete(`/rutas/${id}`),
-  createRutasBulk: async (data) => {
-    return axios.post(`${API_URL}/rutas/bulk`, data);
-  },
+  createRutasBulk: (data) => axiosInstance.post('/rutas/bulk', data),
 
   // Usuarios
   getUsuarios: () => axiosInstance.get('/usuarios'),
   createUsuario: (data) => axiosInstance.post('/usuarios', data),
   updateUsuario: (id, data) => axiosInstance.put(`/usuarios/${id}`, data),
   deleteUsuario: (id) => axiosInstance.delete(`/usuarios/${id}`),
-  createUsuariosBulk: async (data) => {
-    return axios.post(`${API_URL}/usuarios/bulk`, data);
-  },
+  createUsuariosBulk: (data) => axiosInstance.post('/usuarios/bulk', data),
 }; 
